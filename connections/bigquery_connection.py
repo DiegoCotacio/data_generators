00e4a3cb00b978c90b4990f0944edeed0c6f20e0
Diego Cotacio <diego.cotacio@correounivalle.edu.co>
2023-06-27 07:28:45 -0500

    actualizacion credenciales

--- connections/bigquery_connection.py
+++ connections/bigquery_connection.py
@@ -1,10 +1,15 @@
 # bigquery_connection.py
 from google.cloud import bigquery
 from google.oauth2 import service_account
+import os
 
 def connect_to_bigquery():
+
+    # Load BigQuery credentials from the secret
+    credentials_json = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+
     # Load BigQuery credentials from the service_account.json file
-    credentials = service_account.Credentials.from_service_account_file('C:/Users/diego/OneDrive/Escritorio/mlops_projects/ml/churn_syn_data_generator_pipeline/connections/protean-fabric-386717-d6a21dd66382.json')
+    credentials = service_account.Credentials.from_service_account_info(credentials_json)
 
     # Connect to the BigQuery API using the credentials
     client = bigquery.Client(credentials=credentials)
